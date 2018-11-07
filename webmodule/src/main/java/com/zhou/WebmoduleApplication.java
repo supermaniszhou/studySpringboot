dbe78dc9bf6b50f041b282a682f77708925705bd
supermaniszhou <741963634@qq.com>
2018-11-07 09:07:41 +0800

    xiug

--- webmodule/src/main/java/com/zhou/WebmoduleApplication.java
+++ webmodule/src/main/java/com/zhou/WebmoduleApplication.java
@@ -2,10 +2,13 @@ package com.zhou;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
-@MapperScan({"com.zhou.mapper.SqlMapper","com.zhou.mapper.sys"})
+//用于mybatis 扫描mapper 接口
+@MapperScan(basePackages={"com.zhou.mapper.SqlMapper","com.zhou.mapper.sys"})
 public class WebmoduleApplication {
 
 	public static void main(String[] args) {

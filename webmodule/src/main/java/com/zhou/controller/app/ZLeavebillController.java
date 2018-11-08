f3436b530d826b252fed42483ab1d324c8957f77
supermaniszhou <741963634@qq.com>
2018-11-08 16:21:07 +0800

    xiug

--- webmodule/src/main/java/com/zhou/controller/app/ZLeavebillController.java
+++ webmodule/src/main/java/com/zhou/controller/app/ZLeavebillController.java
@@ -2,10 +2,13 @@ package com.zhou.controller.app;
 
 import com.zhou.common.base.BaseController;
 import com.zhou.entity.app.ZLeavebill;
+import com.zhou.entity.sys.SysUser;
 import com.zhou.service.app.ZLeavebillService;
 import com.zhou.util.GenerateUUID;
 import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 
@@ -25,6 +29,9 @@ public class ZLeavebillController extends BaseController {
     @Autowired
     private ZLeavebillService zLeavebillService;
 
+    @Autowired
+    private RedisTemplate<String, Object> template;
+
 
     @RequestMapping(value = "/toZLeavebillList")
     public ModelAndView toZLeavebillList(Model model) {
@@ -34,7 +41,7 @@ public class ZLeavebillController extends BaseController {
     @RequestMapping("/getZLeavebillData")
     @ResponseBody
     public Map<String, Object> getZLeavebillData(@RequestParam(value = "pageSize", required = false) int pageSize,
-                                           @RequestParam(value = "pageIndex", required = false) int pageIndex) {
+                                                 @RequestParam(value = "pageIndex", required = false) int pageIndex) {
 
         Map<String, Object> map = new HashedMap();
         ZLeavebill zLeavebill = new ZLeavebill();
@@ -47,11 +54,21 @@ public class ZLeavebillController extends BaseController {
     }
 
 
-
     @RequestMapping(value = "/doAdd")
     @ResponseBody
-    public Map<String, Object> doAdd(ZLeavebill zLeavebill) {
+    public Map<String, Object> doAdd(HttpServletRequest request, ZLeavebill zLeavebill) {
+        String userId = (String) request.getSession().getAttribute("userId");
         try {
+            SysUser sysUser = null;
+            if (null != userId) {
+                ValueOperations<String, Object> ov = template.opsForValue();
+                //redis 查询操作
+                sysUser = (SysUser) ov.get(userId);
+            }
+            if (null != sysUser) {
+                zLeavebill.setUserId(Integer.toString(sysUser.getId()));
+                zLeavebill.setUserName(sysUser.getRealname());
+            }
             zLeavebill.setId(GenerateUUID.getUUID());
             zLeavebillService.addObj(zLeavebill);
         } catch (Exception e) {

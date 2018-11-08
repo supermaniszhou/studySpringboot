3941a8ee66af09c848131ee7be3ef9d1fcd60ad4
supermaniszhou <741963634@qq.com>
2018-11-08 09:44:05 +0800

    xiug

--- webmodule/src/main/java/com/zhou/WebmoduleApplication.java
+++ webmodule/src/main/java/com/zhou/WebmoduleApplication.java
@@ -4,14 +4,30 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 //用于mybatis 扫描mapper 接口
-@MapperScan(basePackages={"com.zhou.mapper.SqlMapper","com.zhou.mapper.sys"})
+@MapperScan(basePackages = {"com.zhou.mapper.SqlMapper", "com.zhou.mapper.sys"})
 public class WebmoduleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebmoduleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebmoduleApplication.class, args);
+    }
+
+    /**
+     * 设置session失效时间
+     */
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+                configurableEmbeddedServletContainer.setSessionTimeout(1800);
+            }
+        };
+    }
 }

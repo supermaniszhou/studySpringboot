488f9c65abf51954abd04c405d0dd4c22b7c8720
supermaniszhou <741963634@qq.com>
2018-11-13 09:19:29 +0800

    xiug

---
+++ batisgenerator/src/main/java/com/zhou/mapper/SysUserMapper.java
@@ -0,0 +1,17 @@
+package com.zhou.mapper;
+
+import com.zhou.entity.SysUser;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+}

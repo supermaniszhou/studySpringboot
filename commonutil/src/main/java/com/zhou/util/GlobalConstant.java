aebd50719fa62e9200eafe8161a2d634941db461
ll <627465030@qq.com>
2018-11-15 17:35:22 +0800

    update

---
+++ commonutil/src/main/java/com/zhou/util/GlobalConstant.java
@@ -0,0 +1,15 @@
+package com.zhou.util;
+
+/**
+ * @Project : studySpringboot
+ * @Package Name : com.zhou.util
+ * @Description : 全局常量
+ * @Author : lilian
+ * @Creation Date : 2018/11/15 14:25
+ * @ModificationHistory Who When What
+ * _________ ________________ ____________________________________________
+ */
+public class GlobalConstant {
+    public static String FILTER_EXCLUDE_PATH = "/toLogin,/login,/";
+    public static String SESSION_INFO = "zlc_session_info";
+}
